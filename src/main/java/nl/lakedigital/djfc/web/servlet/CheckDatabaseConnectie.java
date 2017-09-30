b6a3addf754d21dadab4ca64295a1b52a2081153
Patrick Heidotting <patrick@lakedigital.nl>
2017-09-30 20:18:14 +0200

    overbodige logging

--- src/main/java/nl/lakedigital/djfc/web/servlet/CheckDatabaseConnectie.java
+++ src/main/java/nl/lakedigital/djfc/web/servlet/CheckDatabaseConnectie.java
@@ -1,6 +1,5 @@
 package nl.lakedigital.djfc.web.servlet;
 
-import nl.lakedigital.djfc.reflection.ReflectionToStringBuilder;
 import nl.lakedigital.djfc.repository.IdentificatieRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +17,7 @@ public class CheckDatabaseConnectie implements Runnable {
     public void run() {
         LOGGER.info("check database connectie");
         identificatieRepository.getSession().getTransaction().begin();
-        LOGGER.debug(ReflectionToStringBuilder.toString(identificatieRepository.getSession().createSQLQuery("/* ping */ SELECT 1").uniqueResult()));
+        identificatieRepository.getSession().createSQLQuery("/* ping */ SELECT 1").uniqueResult();
         identificatieRepository.getSession().getTransaction().commit();
     }
 
